411b8fce0148d27494fc6fe8627d00f13243101c
Aleksandra Karic <aleksandrav86@gmail.com>
2023-10-07 10:14:16 +0200

    Renamed from FormAuthenticationPage.java to LoginPage.java

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -4,18 +4,20 @@ import base.BaseTest;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BaseTest {
 
     public HomePage () {
-        PageFactory.initElements(driver, this);
-    }
-    @FindBy (linkText = "Form Authentication")
-    WebElement linkFormAuthentication;
+        PageFactory.initElements(driver, this);}
 
-    public void linkFormAuthenticationClick (){
-        wdWait.until(ExpectedConditions.visibilityOf(linkFormAuthentication)).click();
-    }
+    @FindBy(linkText = "Form Authentication")
+    WebElement formAuthenticationLink;
 
+    public void FormAuthenticationLinkClick(){
+        wdWait.until(ExpectedConditions.visibilityOf(formAuthenticationLink));
+        actions.scrollToElement(formAuthenticationLink).perform();
+        formAuthenticationLink.click();
+    }
 }
